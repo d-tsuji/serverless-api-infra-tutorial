ed4065c5df50a1144d0126604650bda3d6e82bde
d-tsuji <dram.dt.shonan@gmail.com>
2020-09-23 20:36:55 +0900

    fix: typo

--- lambda_permission.tf
+++ lambda_permission.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_permission" "example_apigateway_lambda" {
   action = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.example_api.arn
+  function_name = aws_lambda_function.example_api.function_name
   principal = "apigateway.amazonaws.com"
 
   source_arn = "${aws_api_gateway_rest_api.example_api.execution_arn}/*/*/*"
